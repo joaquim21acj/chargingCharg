5a1d24baa9eebfaff6f5722fe982aff2e49e7a8b
joaquim21acj <joaquimjunior13@gmail.com>
2021-01-28 13:57:56 -0300

    Revert "Feat/parsing data charger"

--- src/views/supplier_prices_charges.py
+++ src/views/supplier_prices_charges.py
@@ -3,19 +3,12 @@ from flask import Blueprint
 from flask_cors import CORS
 
 supplier_prices_charges_blueprint = Blueprint(
-    'additional', __name__, url_prefix='/api/supplier-charges')
+    'additional', __name__, url_prefix='/api')
 CORS(supplier_prices_charges_blueprint)
 
 
-@supplier_prices_charges_blueprint.route('/raw',
+@supplier_prices_charges_blueprint.route('/supplier-charges',
                                          methods=['GET'])
 def get_supplier_prices_charges():
     res, cod = controller.get_supplier_prices_charges_pretty()
-    return res, cod
-
-
-@supplier_prices_charges_blueprint.route('/charges',
-                                         methods=['GET'])
-def get_charges():
-    res, cod = controller.get_charges_pretty()
     return res, cod
