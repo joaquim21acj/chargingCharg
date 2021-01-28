fe95f3fb7ff0e4c7963c204db8577a0d2c29578c
Joaquim Alves <04763101137@fabrica.local>
2021-01-28 13:49:44 -0300

    Geting and casting only the charge object

--- src/views/supplier_prices_charges.py
+++ src/views/supplier_prices_charges.py
@@ -3,12 +3,19 @@ from flask import Blueprint
 from flask_cors import CORS
 
 supplier_prices_charges_blueprint = Blueprint(
-    'additional', __name__, url_prefix='/api')
+    'additional', __name__, url_prefix='/api/supplier-charges')
 CORS(supplier_prices_charges_blueprint)
 
 
-@supplier_prices_charges_blueprint.route('/supplier-charges',
+@supplier_prices_charges_blueprint.route('/raw',
                                          methods=['GET'])
 def get_supplier_prices_charges():
     res, cod = controller.get_supplier_prices_charges_pretty()
+    return res, cod
+
+
+@supplier_prices_charges_blueprint.route('/charges',
+                                         methods=['GET'])
+def get_charges():
+    res, cod = controller.get_charges_pretty()
     return res, cod
